e5964fc591c8e03badca73ef3dc6aa2f2dd542dd
Omar El Laden <omarzagonel@gmail.com>
2025-12-05 18:50:23 -0300

    correct alpha None return on error

--- src/simulation_config.py
+++ src/simulation_config.py
@@ -65,21 +65,20 @@ class SimulationConfig():
             cls = getattr(module, class_name)
             return cls
         except (ModuleNotFoundError, AttributeError) as e:
-            print("not found", module_name)
             return Task
 
     def get_params_from_file(self):
 
         # Check if file exists
         if not os.path.isfile(self.tasks_path):
-            return f'Could not find file "{self.tasks_path}". Using default scheduling parameters', None, None, None
+            return f'Could not find file "{self.tasks_path}". Using default scheduling parameters', None, None, None, None
 
         with open(self.tasks_path, "r", encoding="utf-8") as f:
             lines = f.readlines()
 
         # Check for minimum file length
         if len(lines) < 2:
-            return f'Not enough lines in file. Using default scheduling parameters', None, None, None
+            return f'Not enough lines in file. Using default scheduling parameters', None, None, None, None
 
 
         line_num = 1
@@ -88,7 +87,7 @@ class SimulationConfig():
         parts = lines[0].strip().split(";")
         parts = [p for p in parts if p != ""]
         if len(parts) != 3:
-            return f"Wrong number of parameters in line {line_num}. Using default parameters", None, None, None
+            return f"Wrong number of parameters in line {line_num}. Using default parameters", None, None, None, None
 
 
         # Extract parameters from file
@@ -97,21 +96,21 @@ class SimulationConfig():
         alg_scheduling = lines[0].split(";")[0].lower().strip()
         list_alg = self.find_algorithms()
         if not alg_scheduling in list_alg:
-            return f'Invalid algorithm "{alg_scheduling}" in line {line_num}. Options are {list_alg}. Using default parameters', None, None, None
+            return f'Invalid algorithm "{alg_scheduling}" in line {line_num}. Options are {list_alg}. Using default parameters', None, None, None, None
 
         # Quantum
         quantum = lines[0].split(";")[1].strip()
         if quantum.isdigit() and int(quantum) != 0:
             quantum = int(quantum)
         else:
-            return f"Invalid quantum in line {line_num}. Using default parameters", None, None, None
+            return f"Invalid quantum in line {line_num}. Using default parameters", None, None, None, None
 
         # Alpha
         alpha = lines[0].split(";")[2].strip()
         if alpha.isdigit() and int(alpha) != 0:
             alpha = int(alpha)
         else:
-            return f"Invalid alpha in line {line_num}. Using default parameters", None, None, None
+            return f"Invalid alpha in line {line_num}. Using default parameters", None, None, None, None
 
 
         line_num += 1
@@ -122,42 +121,43 @@ class SimulationConfig():
 
             # Check line size
             parts = line.strip().split(";")
-            parts = [p for p in parts if p != ""]
+            parts = [p for p in parts if p != ""]  # ignore empty fields
             if len(parts) < 5:
-                return f"Not enough task parameters in line {line_num}. Using default parameters", None, None, None
+                return f"Not enough task parameters in line {line_num}. Using default parameters", None, None, None, None
 
             # Task id
             task_id = parts[0].replace("t", "").strip()
             if task_id.isdigit() and int(task_id) != 0:
                 task_id = int(task_id)
             else:
-                return f"Invalid task id in line {line_num}. Using default parameters", None, None, None
+                return f"Invalid task id in line {line_num}. Using default parameters", None, None, None, None
 
             # Color code
             task_color_hex = parts[1].strip().lstrip("#").lower()
             if not re.fullmatch(r"[0-9a-f]{6}", task_color_hex):
-                return f"Invalid task color code in line {line_num}. Using default parameters", None, None, None
+                return f"Invalid task color code in line {line_num}. Using default parameters", None, None, None, None
 
             # Start time
             task_start_time = parts[2].strip()
             if task_start_time.isdigit():
                 task_start_time = int(task_start_time)
             else:
-                return f"Invalid task start time in line {line_num}. Using default parameters", None, None, None
+                return f"Invalid task start time in line {line_num}. Using default parameters", None, None, None, None
 
             # Duration
             task_duration = parts[3].strip()
             if task_duration.isdigit() and int(task_duration) != 0:
                 task_duration = int(task_duration)
             else:
-                return f"Invalid task duration in line {line_num}. Using default parameters", None, None, None
+                return f"Invalid task duration in line {line_num}. Using default parameters", None, None, None, None
 
             # Priority
             task_priority = parts[4].strip()
             if task_priority.isdigit() and int(task_priority) != 0:
                 task_priority = int(task_priority)
             else:
-                return f"Invalid task priority in line {line_num}. Using default parameters", None, None, None
+                return f"Invalid task priority in line {line_num}. Using default parameters", None, None, None, None
+
 
             class_task = self.import_task(alg_scheduling)
             task = class_task(
